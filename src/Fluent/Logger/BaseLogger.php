6af7e1d8641d16bb9bb19e9543ce64f944f1ee13
Sotaro KARASAWA <sotarok@crocos.co.jp>
2012-02-26 02:26:14 +0900

    registerErrorHandler() throws \InvalidArgumentException instead of \Exception

--- src/Fluent/Logger/BaseLogger.php
+++ src/Fluent/Logger/BaseLogger.php
@@ -50,16 +50,16 @@ abstract class BaseLogger implements \Fluent\Logger
     }
 
     /**
-     * @param mixed $callable function name, array or closure are OK.
+     * @param  callable  $callable function name, array or closure
      * @return bool
-     * @throws \Exception
+     * @throws \InvalidArgumentException
      */
     public function registerErrorHandler($callable)
     {
         if (is_callable($callable)) {
             $this->error_handler = $callable;
         } else {
-            throw new \Exception("could not register error handler");
+            throw new \InvalidArgumentException("Error handler must be callable.");
         }
         return true;
     }
